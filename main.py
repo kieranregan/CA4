b11642252e5f458517ef2cbac2a33b33899cdb97
Kieran Regan <kieranregan87@gmail.com>
2018-10-31 22:27:19 +0000

    program files

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from process_changes import Processer
+from process_changes_with_object import Commit
 
-FileProcesser = Processer()
+FileProcesser = Commit("Revision","Author","Date","Time","NoOfLines")
 
-Data = FileProcesser.read_file("changes_python.log.txt")
+Data = FileProcesser.read_file("changes_python.txt")
 
-Commits = FileProcesser.getCommits(Data)
+Commits = FileProcesser.get_commits(Data)
 
-FileProcesser.save_commits(Data, "output.csv")
+FileProcesser.save_commits(Commits, "output.csv")
 
 
 
